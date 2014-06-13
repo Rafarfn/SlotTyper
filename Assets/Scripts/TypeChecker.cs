1b5d98a76f085b79b569fb62e5c20403a884f331
Sergio Jiménez <sergiojj932@gmail.com>
2014-06-13 09:41:51 +0200

    initialization

--- Assets/Scripts/TypeChecker.cs
+++ Assets/Scripts/TypeChecker.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TypeChecker : MonoBehaviour {
+public class TypeChecker : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public string stringToBeChecked ="pera";
+    private string currenteCheckedString="asd";
+    public bool isCorrect = true;
+    private int wordCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // GetKeys();
+        if (Input.anyKeyDown)
+        {
+            Debug.Log(Input.inputString);
+        }
+    }
+
+    private void GetKeys()
+    {
+        if (Input.anyKeyDown )
+        {
+            currenteCheckedString += Input.inputString;
+            Debug.Log(Input.inputString);
+           /* if (currenteCheckedString[wordCounter].Equals(stringToBeChecked[wordCounter]))
+            {
+                isCorrect = true;
+            }
+            else
+            {
+                
+                isCorrect = false;
+            }*/
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50,50,100,20), currenteCheckedString);
+    }
 }
