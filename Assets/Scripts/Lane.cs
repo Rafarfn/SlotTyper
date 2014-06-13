53201bc6b9d3da9fdc95521ec2c8bae892d7149a
Rafa <rafa.delriego@crazybitsstudios.com>
2014-06-13 11:37:40 +0200

    Plate, Item and Lane scripts added Plates stop the Items when they collide with them or other items queued Lanes send Items each X seconds

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents "paths" where one plate is placed, and items fall towards it.
+/// </summary>
+public class Lane : MonoBehaviour
+{
+	/// <summary>
+	/// The plate on this lane
+	/// </summary>
+	public Plate plate;
+
+	/// <summary>
+	/// Time between two consecutive items.
+	/// </summary>
+	public float timeBetweenItems = 15.0f;
+	protected float timeSinceLastItem = 0;
+
+	/// <summary>
+	/// Prefab of item to clone.
+	/// TODO: An ItemManager will deal with tihs
+	/// </summary>
+	public GameObject itemPrefab;
+
+	/// <summary>
+	/// Used to mark the position where the items got created
+	/// </summary>
+	public GameObject generationPlaceholder;
+
+
+	public void CreateItem ()
+	{
+		if (plate != null)
+		{
+			GameObject itemObject = GameObject.Instantiate(itemPrefab, generationPlaceholder.transform.position, Quaternion.identity) as GameObject;
+			plate.OnItemStartToFall(itemObject.GetComponent<Item>());
+
+			// Set a movement direction for the item
+			Item item = itemObject.GetComponent<Item>();
+			if (item != null)
+			{
+				// Move towards the plate form the starting position
+				item.movementDirection = plate.transform.position - generationPlaceholder.transform.position;
+			}
+		}
+	}
+
+
+	void Update ()
+	{
+		timeSinceLastItem += Time.deltaTime;
+		if (timeSinceLastItem >= timeBetweenItems)
+		{
+			timeSinceLastItem = 0;
+			CreateItem();
+		}
+	}
+
+}
--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -1,15 +1,173 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Plate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+/// <summary>
+/// Script for the plates in the middle of the screen, which get lowered when the Items
+/// get dropped on it
+/// </summary>
+public class Plate : MonoBehaviour
+{
+
+	/// <summary>
+	/// The currently selected plate
+	/// </summary>
+	public static Plate currentPlate;
+
+	/// <summary>
+	/// Current list of items in the 
+	/// </summary>
+	protected List<Item> items = new List<Item>();
+
+	/// <summary>
+	/// Items that are falling in the same lane, but have not reached
+	/// the plate (yet).
+	/// </summary>
+	protected Queue<Item> itemsFalling = new Queue<Item>();
+
+
+	/// <summary>
+	/// The plate accessed when the wants to go to another plate from this one.
+	/// </summary>
+	public Plate nextPlate;
+
+	/// <summary>
+	/// The initial position, where the plate is when it has no items over it
+	/// </summary>
+	public Vector3 standByPosition;
+
+	/// <summary>
+	/// The size of every step.
+	/// </summary>
+	public Vector3 stepSize;
+
+
+
+	void Start ()
+	{
+		standByPosition = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	void Update ()
+	{
+		// TODO: Move to the position depending on the current number of items queued
+		int itemsOnPlate = items.Count;
+
+		if (itemsOnPlate > 0)
+		{
+			// Move the plate to a position according to the number of elements queued
+			//transform.Translate(Vector3.Lerp(transform.position, standByPosition - stepSize * itemsOnPlate, Time.deltaTime));
+
+		}
+
 	}
+
+
+	void OnDestroy ()
+	{
+		if (currentPlate == this)	currentPlate = null;
+	}
+
+
+	/// <summary>
+	/// Returns the next item in the queue, or the one that is falling to this
+	/// plate. Returns null if there is no Item
+	/// </summary>
+	/// <returns>The next item.</returns>
+	public Item GetNextItem ()
+	{
+		if (items.Count == 0)
+		{
+			if (itemsFalling.Count == 0)	return null;
+
+			return itemsFalling.Peek();
+		}
+
+		return items[0];
+	}
+
+	/// <summary>
+	/// Called when the last item in the queue was typed properly.
+	/// Also, if the item was in the queue of items already in the plate,
+	/// it gets raised
+	/// </summary>
+	public void ItemTypedRight ()
+	{
+		Item itemDestroyed = null;
+		if (items.Count == 0)
+		{
+			if (itemsFalling.Count == 0)	return;
+
+			itemDestroyed = itemsFalling.Dequeue();
+		}
+		else
+		{
+			itemDestroyed = items[0];
+			items.RemoveAt(0);
+
+			// Unsubscribe from its listener, and subscribe to the first falling (if any)
+			itemDestroyed.fallListeners -= OnItemFell;
+			if (itemsFalling.Count > 0)
+			{
+				itemsFalling.Peek().fallListeners += OnItemFell;
+			}
+		}
+
+		if (itemDestroyed != null)
+		{
+			itemDestroyed.Destroy();
+		}
+	}
+
+	/// <summary>
+	/// Called when the last item in the queue was typed wrong.
+	/// </summary>
+	public void ItemTypedWrong ()
+	{
+		// TODO
+		Debug.Log("Item typed wrongly");
+	}
+
+	/// <summary>
+	/// When an item fell to the plate or an item queued over it, this method
+	/// gets called. It moves the first item that is falling to the queue of items
+	/// </summary>
+	protected void OnItemFell (Item itemThatFell)
+	{
+		if (itemsFalling.Count == 0)	return;
+		
+		// Unsubscribe form the last item queued
+		if (items.Count > 0)
+		{
+			items[items.Count - 1].fallListeners -= OnItemFell;
+		}
+
+		// Grab the first item falling and pass it to the items queued
+		Item lastItemQueued = itemsFalling.Dequeue();
+		lastItemQueued.isStopped = true;
+		
+		// Subscribe to its fallListeners, and queue it
+		lastItemQueued.fallListeners += OnItemFell;
+		items.Add(lastItemQueued);
+	}
+
+	/// <summary>
+	/// Called when an item starts to fall to this plate
+	/// </summary>
+	/// <param name="item">Item.</param>
+	public void OnItemStartToFall (Item item)
+	{
+		if (item == null)	return;
+
+		itemsFalling.Enqueue(item);
+
+		// Subscribe to its listeners, if there are no items currently queued
+		if (items.Count == 0)
+		{
+			item.fallListeners += OnItemFell;
+		}
+	}
+
 }
